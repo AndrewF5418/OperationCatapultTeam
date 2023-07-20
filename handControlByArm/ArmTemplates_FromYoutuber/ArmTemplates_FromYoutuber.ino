eecc517332996f45ee3a86bfa546106a184b6744
AndrewF5418 <fang13612978925@outlook.com>
2023-07-20 23:44:29 +0800

    Resource Integration
    2023/7/20 11:43 in USA

--- handControlByArm/ArmTemplates_FromYoutuber/ArmTemplates_FromYoutuber.ino
+++ handControlByArm/ArmTemplates_FromYoutuber/ArmTemplates_FromYoutuber.ino
@@ -15,65 +15,66 @@ Servo indexfinger_Servo;
 Servo middlefinger_Servo;
 Servo ringfinger_Servo;
 Servo littlefinger_Servo;
-
+Servo SERVO_1;
 /*-------------------------------- void setup ------------------------------------------------*/
-
+int thresholdCounter=0;
+int thresholdCounterTwice=0;
 void setup(){
   
   //BAUDRATE set to 115200, remember it to set monitor serial properly. 
   //Used this Baud Rate and "NL&CR" option to visualize the values correctly.
   Serial.begin(115200);
   
-  thumb_Servo.attach(3);//ID for thumb to little finger in orders is 1 to 5. Set up the attach port (control cable), the port named in numbers
-  indexfinger_Servo.attach(5);
-  middlefinger_Servo.attach(6);
-  ringfinger_Servo.attach(9);
-  littlefinger_Servo.attach(10);
+  // thumb_Servo.attach(3);//ID for thumb to little finger in orders is 1 to 5. Set up the attach port (control cable), the port named in numbers
+  // indexfinger_Servo.attach(5);
+  // middlefinger_Servo.attach(6);
+  // ringfinger_Servo.attach(9);
+  // littlefinger_Servo.attach(10);
+  SERVO_1.attach(3);
   //Set servo motor to digital pin 3
   
 }
 
 /*--------------------------------  void loop  ------------------------------------------------*/
 
+
 void loop(){
 
   //The "Value" variable reads the value from the analog pin to which the sensor is connected.
-  value1 = analogRead(A0);
-  //int value2 = analogRead(A1);
-  //method 1
-  //int servoPos = 170; //stretch value
-  //int servoReturn = 10;
-  //If the sensor value is GREATER than the THRESHOLD, the servo motor will turn to 170 degrees.
-  // if(value1 > THRESHOLD){
-  //   SERVO_1.write(servoPos);
-  //   thumb_Servo.write(servoPos);
-  //   indexfinger_Servo.write(servoPos);
-  //   middlefinger_Servo.write(servoPos);
-  //   ringfinger_Servo.write(servoPos);
-  //   littlefinger_Servo.write(servoPos);
-  // }
-  // //If the sensor is LESS than the THRESHOLD, the servo motor will turn to 10 degrees.
-  // else{
-  //   SERVO_1.write(servoReturn);
-  // }
-
-  //You can use serial monitor to set THRESHOLD properly, comparing the values shown when you open and close your hand.
-}
-void loop(){
-  //method 2
-  //map(value, fromLow, fromHigh, toLow, toHigh)
-  // int servoPos = map(value1, 0, 1023, 0,180);
-
-  //control individual fingers with emg
-  // thumb_Servo.write(servoPos);
-  // indexfinger_Servo.write(servoPos);
-  // middlefinger_Servo.write(servoPos);
-  // ringfinger_Servo.write(servoPos);
-  // littlefinger_Servo.write(servoPos);
+  int value1 = analogRead(A1);
   
-  //monitoring
+  //method 1
+  
+  int servoPos = 0; //stretch value
+  int servoReturn = 10;
+  int THRESHOLD = 340;
+  
   Serial.println(value1);
-  //Serial.print(",");//Separate two EMG valuesS
-  //Serial.println(value2);
-  delay(1);
+  //If the sensor value is GREATER than the THRESHOLD, the servo motor will turn to 170 degrees.
+  if(value1 > THRESHOLD){
+ 
+  Serial.print(",");//Separate two EMG valuesS
+  Serial.println(value1);
+ 
+      for (servoPos = 180; servoPos>=0; servoPos +=1){
+        SERVO_1.write(servoPos);
+        Serial.print(",");//Separate two EMG valuesS
+  Serial.println(value1);
+        delay(10);
+      }
+     delay(10); 
+  }
+    //If the sensor is LESS than the THRESHOLD, the servo motor will turn to 10 degrees.
+  else{
+    SERVO_1.write(servoReturn);
+    for (servoPos = 60; servoPos>=0; servoPos -=1){
+        SERVO_1.write(servoPos);
+        Serial.print(",");//Separate two EMG valuesS
+  Serial.println(value1);
+        delay(10);
+      }
+    delay(10);
+  }
+  delay(25);
 }
+
